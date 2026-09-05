6d234ef2c7ec50a753fc425d545ff79b80836024
Erez Bashan <erez.bashan@gmail.com>
2026-09-05 11:46:25 +0300

    By Antigravity: Revert trainer to 2-player to replace the hallucinated data first

--- games/king-of-tokyo/src/bots/bucketConfig.ts
+++ games/king-of-tokyo/src/bots/bucketConfig.ts
@@ -1240,16 +1240,72 @@ export const bucketConfig: Record<string, any> = {
   },
   "P:3|VP:16-19|OVP:16-19|HLT:8+|OHLT:5-7|TOK:false": {
     "VPS": false,
-    "ATT": false,
-    "HLT": true,
+    "ATT": true,
+    "HLT": false,
     "ENR": false,
     "YLD": true
   },
   "P:3|VP:16-19|OVP:16-19|HLT:8+|OHLT:8+|TOK:true": {
     "VPS": false,
-    "ATT": false,
+    "ATT": true,
+    "HLT": false,
+    "ENR": false,
+    "YLD": false
+  },
+  "P:3|VP:16-19|OVP:16-19|HLT:8+|OHLT:8+|TOK:false": {
+    "VPS": false,
+    "ATT": true,
+    "HLT": false,
+    "ENR": true,
+    "YLD": true
+  },
+  "P:3|VP:16-19|OVP:10-15|HLT:1-4|OHLT:1-4|TOK:true": {
+    "VPS": false,
+    "ATT": true,
+    "HLT": true,
+    "ENR": false,
+    "YLD": false
+  },
+  "P:3|VP:16-19|OVP:10-15|HLT:1-4|OHLT:1-4|TOK:false": {
+    "VPS": false,
+    "ATT": true,
+    "HLT": true,
+    "ENR": false,
+    "YLD": false
+  },
+  "P:3|VP:16-19|OVP:10-15|HLT:1-4|OHLT:5-7|TOK:true": {
+    "VPS": true,
+    "ATT": true,
     "HLT": true,
     "ENR": true,
     "YLD": false
+  },
+  "P:3|VP:16-19|OVP:10-15|HLT:1-4|OHLT:5-7|TOK:false": {
+    "VPS": true,
+    "ATT": true,
+    "HLT": false,
+    "ENR": false,
+    "YLD": true
+  },
+  "P:3|VP:16-19|OVP:10-15|HLT:1-4|OHLT:8+|TOK:true": {
+    "VPS": true,
+    "ATT": true,
+    "HLT": true,
+    "ENR": false,
+    "YLD": false
+  },
+  "P:3|VP:16-19|OVP:10-15|HLT:1-4|OHLT:8+|TOK:false": {
+    "VPS": true,
+    "ATT": true,
+    "HLT": true,
+    "ENR": true,
+    "YLD": false
+  },
+  "P:3|VP:16-19|OVP:10-15|HLT:5-7|OHLT:1-4|TOK:true": {
+    "VPS": true,
+    "ATT": false,
+    "HLT": true,
+    "ENR": true,
+    "YLD": true
   }
 };
